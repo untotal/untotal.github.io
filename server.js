c07791579e58910f1cedef2ac419354e69140b8f
Krishna Jha <krishna@antarix.com>
2024-03-06 13:55:14 +0530

    - added sepaarte css examples, including a sample email compose panel div like in gmail - added async html page to fetch data from our server and to display like a possible gmail inbox
    TODO:
    - move the compose out into the async page
    - rename async page to email
    - get sample data from web/AI for email objects in an array
    - fetch emails from our server (instead of users) and display as email
    - buold more on this example...using this email page as a standalone web app which can possibly become a public project

--- server.js
+++ server.js
@@ -15,6 +15,35 @@ app.get('/js', (req, res) => {
     //res.send('Hello World!')
 })
 
+app.get('/css', (req, res) => {
+  res.sendFile(path.join(__dirname, '/css.html'));
+  //res.send('Hello World!')
+})
+
+app.get('/fetch', (req, res) => {
+  res.sendFile(path.join(__dirname, '/async.html'));
+  //res.send('Hello World!')
+})
+
+
+
+app.get('/api/getUsers', (req, res) => {
+
+  var users = {"users":[    
+    {"name":"Ram", "email":"ram@gmail.com", "age":23},    
+    {"name":"Shyam", "email":"shyam23@gmail.com", "age":28},  
+    {"name":"John", "email":"john@gmail.com", "age":33},    
+    {"name":"Bob", "email":"bob32@gmail.com", "age":41}   
+  ]}  
+  
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(users));
+
+})
+
+
+
+
 
 app.get('/resume', (req, res) => {
     res.send('resume')
